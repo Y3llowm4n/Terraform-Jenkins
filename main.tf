e6d27e50d554b6df30aea1fab50220fb85d8ad8d
Y3llowm4n <mikegeelen2002@gmail.com>
2023-12-22 12:05:39 +0100

    changed code

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "public_instance" {
   ami           = var.ami
   instance_type = var.instance_type
-
+  subnet_id     = aws_subnet.vpc_1_subnet_b.id
   tags = {
     Name = var.name_tag,
   }
