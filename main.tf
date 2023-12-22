99b1c7a8bb5871292f32a0b92176d83c95f1852f
Y3llowm4n <mikegeelen2002@gmail.com>
2023-12-22 11:57:28 +0100

    changed code

--- main.tf
+++ main.tf
@@ -1,12 +1,8 @@
-provider "aws" {
-  region = "eu-central-1"
-}
+resource "aws_instance" "public_instance" {
+  ami           = var.ami
+  instance_type = var.instance_type
 
-resource "aws_instance" "Demo" {
-  ami           = "ami-0faab6bdbac9486fb"
-  instance_type = "t2.micro"
-  key_name      = "key1"
   tags = {
-    Name = "Demo1"
+    Name = var.name_tag,
   }
 }
---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  default     = ""
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  default     = ""
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-central-1"
+}
+
+
+variable "ami" {
+  type        = string
+  description = "Ubuntu AMI ID"
+  default     = "ami-0faab6bdbac9486fb"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Instance type"
+  default     = "t2.micro"
+}
+
+variable "name_tag" {
+  type        = string
+  description = "Name of the EC2 instance"
+  default     = "My EC2 Instance"
+}
