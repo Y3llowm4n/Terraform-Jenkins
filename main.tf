4b2e34641e1a2b5fc3ddb1fa59076679e8999bf6
Y3llowm4n <mikegeelen2002@gmail.com>
2023-12-22 11:51:29 +0100

    changed code

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "Demo" {
-  ami           = "ami-0fc5d935ebf8bc3bc"
+  ami           = "ami-0faab6bdbac9486fb"
   instance_type = "t2.micro"
   key_name      = "key1"
   tags = {
